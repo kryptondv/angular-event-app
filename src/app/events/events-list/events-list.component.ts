ea4ca7c274d29f15eabf30f4710d8100edeb7354
kryptondv <kryptondevpl@gmail.com>
2021-04-01 20:35:57 +0200

    added events list component

---
+++ src/app/events/events-list/events-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-events-list',
+  templateUrl: './events-list.component.html',
+  styleUrls: ['./events-list.component.scss']
+})
+export class EventsListComponent implements OnInit {
+  event = {
+    id:1,
+    name: 'Angular Connect',
+    date: '14/04/2021',
+    time: '10:00 am',
+    price: 599.99,
+    imageUrl: '/assets/images/angularconnect-shield.png',
+    location: {
+      address: '1057 DT',
+      city: 'London',
+      country: 'England'
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
