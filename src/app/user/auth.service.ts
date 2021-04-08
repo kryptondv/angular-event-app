0e5e24f6f0b06d57a1316297caeb0ead0dc9c478
kryptondv <kryptondevpl@gmail.com>
2021-04-08 14:53:17 +0200

    added auth service

---
+++ src/app/user/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { IUser } from './user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  currentUser: IUser;
+
+  constructor() {}
+
+  loginUser(userName: string, password: string) {
+    this.currentUser = {
+      id: 1,
+      userName,
+      firstName: 'John',
+      lastName: 'Wayne',
+    };
+  }
+
+  get isAuthenticated() {
+    return !!this.currentUser;
+  }
+}
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
+interface IFormValues {
+  userName: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-login',
@@ -7,11 +12,11 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
 
-  login(form: NgForm) {
-    console.log(form);
+  login(formValues: IFormValues) {
+    this.authService.loginUser(formValues.userName, formValues.password);
   }
 }
