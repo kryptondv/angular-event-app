b3b0cb2220ceeb3e951f71aeca788e2cd1eb5caf
kryptondv <kryptondevpl@gmail.com>
2021-04-18 19:28:06 +0200

    refactored collapsible-well component

--- src/app/common/collapsible-well/collapsible-well.component.ts
+++ src/app/common/collapsible-well/collapsible-well.component.ts
@@ -6,7 +6,6 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./collapsible-well.component.scss']
 })
 export class CollapsibleWellComponent implements OnInit {
-  @Input() title: string;
   showContent = false;
   constructor() { }
 
